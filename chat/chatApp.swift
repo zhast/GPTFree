9af0357c374943d9d93ef7d9913f88974620acf5
Steven Zhang <stevezzhang@gmail.com>
2025-11-13 11:10:27 -0800

    Initial Commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Steven Zhang on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
